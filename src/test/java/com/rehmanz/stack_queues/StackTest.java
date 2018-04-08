c7c6c7cc28b43808e8d233a0c82b19df2645ab7f
Zile Rehman <zile@nyansa.com>
2018-04-08 11:56:35 -0700

    Negative test case for Travis CI.

---
+++ src/test/java/com/rehmanz/stack_queues/StackTest.java
@@ -0,0 +1,60 @@
+package com.rehmanz.stack_queues;
+
+import edu.princeton.cs.algs4.stdlib.In;
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
+
+public class StackTest {
+    private static final Logger logger = Logger.getLogger(StackTest.class);
+    String  dataDir = new File(".").getAbsolutePath() + "/src/test/resources/stack/",
+            dataInputFile = "toBeInput.txt",
+            dataExpectedFile = "toBeExpected.txt";
+    In dataInput,
+       dataExpected;
+    LinkedStackOfStrings stack = new LinkedStackOfStrings();
+
+    @Before
+    public void setUp() throws Exception {
+        try {
+            logger.debug(dataDir);
+            dataInput = new In(dataDir+dataInputFile);
+            dataExpected = new In(dataDir+dataExpectedFile);
+
+        }
+        catch (Exception e) { logger.error(e); }
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        dataInput.close();
+        dataExpected.close();
+    }
+
+    @Test
+    public void testStack() {
+        logger.info("input: " + dataInput);
+        while (!dataInput.isEmpty()) {
+            String item = dataInput.readString();
+            if (item.equals("-")) {
+                stack.pop();
+            } else {
+                stack.push(item);
+            }
+        }
+
+        String dataActual = "";
+        while (!stack.isEmpty()) {
+            dataActual += " " + (stack.pop());
+        }
+
+        assertEquals("Actual and expected values don't match",
+                     dataActual.toString().trim(),
+                     dataExpected.readAll().trim());
+    }
+}
